db3ae4139492ea5fa6db65fe0c045e0bdfc4383e
Oliyy <under5zzs@gmail.com>
2018-07-15 19:45:50 -0700

    Added package.json

---
+++ config.js
@@ -0,0 +1,6 @@
+{
+  name: "WeatherMan",
+  token: "",
+  prefix: "weatherman",
+  botLeader: ""
+}
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js');
+const Enmap = require("enmap");
 const fs = require('file-system';)
 
 const client = new Discord.Client();
 const config = require('./config.json');
 client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
